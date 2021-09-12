a3104ed494541a4d820c4571b1eeeb9d11a4ea18
Michael Maenz <michael.maenz@iconten.de>
2021-09-12 21:17:09 +0200

    initial spring boot project setup

---
+++ src/test/java/de/ragnaruk/dcbot/DcbotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ragnaruk.dcbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
