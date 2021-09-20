10e6c26e528e55426682393ff9f4e685cf797919
mmaenz <michael.maenz@iconten.de>
2021-09-20 13:52:21 +0200

    BOT_TOKEN by discord.properties

--- src/main/java/de/ragnaruk/dcbot/DcBotApplication.java
+++ src/main/java/de/ragnaruk/dcbot/DcBotApplication.java
@@ -4,6 +4,10 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.interaction.SlashCommandEvent;
 import discord4j.rest.RestClient;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -15,14 +19,28 @@ import reactor.core.publisher.Mono;
 @SpringBootApplication
 public class DcBotApplication {
 	
+	public Properties loadProperties(String file) {
+	    Properties props = new Properties();
+	    ClassLoader classLoader = getClass().getClassLoader();
+	    try (InputStream fis = classLoader.getResourceAsStream(file)) {
+	    	props.load(fis);
+	    }
+	    catch (IOException ioex) { 
+	    	ioex.printStackTrace(); 
+	    }
+	    return props;
+	}
+
+	private static String BOT_TOKEN = new DcBotApplication().loadProperties("discord.properties").getProperty("BOT_TOKEN");
+	
     public static void main(String[] args) {
-        //Start spring application
+    	//Start spring application
         ApplicationContext springContext = new SpringApplicationBuilder(DcBotApplication.class)
             .build()
             .run(args);
 
         //Login
-        DiscordClientBuilder.create(System.getenv("BOT_TOKEN")).build()
+        DiscordClientBuilder.create(BOT_TOKEN).build()
             .withGateway(gatewayClient -> {
                 SlashCommandListener slashCommandListener = new SlashCommandListener(springContext);
 
@@ -35,6 +53,6 @@ public class DcBotApplication {
 
     @Bean
     public RestClient discordRestClient() {
-        return RestClient.create(System.getenv("BOT_TOKEN"));
+        return RestClient.create(BOT_TOKEN);
     }
 }
